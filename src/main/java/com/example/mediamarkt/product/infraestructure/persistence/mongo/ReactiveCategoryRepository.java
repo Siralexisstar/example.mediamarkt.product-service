6f2f4868cb263ce93bcf8e881454605411812d96
SiralexisStar <alexgarciavillar96@gmail.com>
2026-04-30 01:30:12 +0200

    redirect files to correct folder

--- src/main/java/com/example/mediamarkt/product/infraestructure/persistence/mongo/ReactiveCategoryRepository.java
+++ src/main/java/com/example/mediamarkt/product/infraestructure/persistence/mongo/ReactiveCategoryRepository.java
@@ -1,8 +1,6 @@
-package com.example.mediamarkt.product.infraestructure.persistence;
+package com.example.mediamarkt.product.infraestructure.persistence.mongo;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.example.mediamarkt.product.infraestructure.persistence.mongo.CategoryDocument;
 
 public interface ReactiveCategoryRepository extends ReactiveMongoRepository<CategoryDocument, String> {
 
--- src/main/java/com/example/mediamarkt/product/infraestructure/persistence/mongo/ReactiveProductRepository.java
+++ src/main/java/com/example/mediamarkt/product/infraestructure/persistence/mongo/ReactiveProductRepository.java
@@ -1,8 +1,6 @@
-package com.example.mediamarkt.product.infraestructure.persistence;
+package com.example.mediamarkt.product.infraestructure.persistence.mongo;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.example.mediamarkt.product.infraestructure.persistence.mongo.ProductDocument;
 
 public interface ReactiveProductRepository extends ReactiveMongoRepository<ProductDocument, String> {
 
